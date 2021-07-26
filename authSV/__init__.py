9e0733717f8716df88aa0d1b145bc0e569f76f63
Sayan Biswas <sayan@pokurt.me>
2021-07-26 16:02:31 +0530

    Add validation
    needs more work

--- authSV/__init__.py
+++ authSV/__init__.py
@@ -1,2 +1,2 @@
-from authSV.methods import GenChallenge
-__all__ = [GenChallenge]
+from authSV.methods import GenChallengeAnswer, Validate
+__all__ = [GenChallengeAnswer, Validate]
--- authSV/methods.py
+++ authSV/methods.py
@@ -1,11 +1,24 @@
+import base64
+
 from authSV.helpers import encode, decode, GetTOTP
 
 
-def GenChallenge(challenge_hash, client_private_hash):
+def GenChallengeAnswer(challenge_hash: str, client_private_hash: str) -> str:
     data = {
         "challenge_hash": challenge_hash,
         "client_private_hash": client_private_hash,
     }
-
+    # print(client_private_hash.encode("utf-8"))
     key = GetTOTP(b"SV")
     return encode(key=bytes(key, 'utf-8'), data=str(data).encode("utf-8"))
+
+
+def Validate(challenge_answer: str, client_private_hash: str):
+    key = GetTOTP(b"SV")
+    r = None
+    try:
+        r = decode(enc=challenge_answer, key=bytes(key, 'utf-8'))
+    except Exception:
+        return ""
+    if r:
+        return base64.urlsafe_b64encode(bytes(r + client_private_hash, "utf-8"))
